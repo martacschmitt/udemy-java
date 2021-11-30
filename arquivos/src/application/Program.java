4993047dc05bea8c66c79472eebf0bed4ca4bf92
Marta Schmitt <mcristina.schmitt@gmail.com>
2021-11-29 21:54:02 -0300

    Informacoes do caminho do arquivo

--- arquivos/src/application/Program.java
+++ arquivos/src/application/Program.java
@@ -14,20 +14,9 @@ public class Program {
 		
 		File path = new File(strPath);
 		
-		File[] folders = path.listFiles(File::isDirectory);
-		System.out.println("FOLDERS:");
-		for (File folder : folders) {
-			System.out.println(folder);
-		}
-		
-		File[] files = path.listFiles(File::isFile);
-		System.out.println("FILES:");
-		for (File file : files) {
-			System.out.println(file);
-		}
-		
-		boolean success = new File(strPath + "\\subdir").mkdir();
-		System.out.println("Directory created succesfully: " + success);
+		System.out.println("getName: " + path.getName());
+		System.out.println("getParent: " + path.getParent());
+		System.out.println("getPath: " + path.getPath());
 		
 		sc.close();
 		
