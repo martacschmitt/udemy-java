77900f92c0c4f0630fa2e66145c107284ea431ca
Marta Schmitt <mcristina.schmitt@gmail.com>
2021-12-02 09:37:19 -0300

    Exemplo de heranca sem interface

--- interfaces/src/application/Program.java
+++ interfaces/src/application/Program.java
@@ -1,49 +1,21 @@
 package application;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-import java.util.Locale;
-import java.util.Scanner;
-
-import model.entities.CarRental;
-import model.entities.Vehicle;
-import model.services.BrazilTaxService;
-import model.services.RentalService;
+import model.entities.Circle;
+import model.entities.Rectangle;
+import model.entities.Shape;
+import model.enums.Color;
 
 public class Program {
 
-	public static void main(String[] args) throws ParseException {
+	public static void main(String[] args) {
 		
-		Locale.setDefault(Locale.US);		
-		Scanner sc = new Scanner(System.in);
-		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:ss");
+		Shape s1 = new Circle(Color.BLACK, 2.0);
+		Shape s2 = new Rectangle(Color.WHITE, 3.0, 4.0);
 		
-		System.out.println("Enter rental data");
-		System.out.print("Car model: ");
-		String carModel = sc.nextLine();
-		System.out.print("Pickup (dd/MM/yyyy hh:ss): ");
-		Date start = sdf.parse(sc.nextLine());
-		System.out.print("Return (dd/MM/yyyy hh:ss): ");
-		Date finish = sdf.parse(sc.nextLine());
-		
-		CarRental cr = new CarRental(start, finish, new Vehicle(carModel));
-		
-		System.out.print("Enter price per hour: ");
-		double pricePerHour = sc.nextDouble();
-		System.out.print("Enter price per day: ");
-		double pricePerDay = sc.nextDouble();
-		
-		RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
-		
-		rentalService.processInvoice(cr);
-		
-		System.out.println("INVOICE:");
-		System.out.println("Basic payment: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
-		System.out.println("Tax: " + String.format("%.2f", cr.getInvoice().getTax()));
-		System.out.println("Total payment: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
-		
-		sc.close();
+		System.out.println("Circle color: " + s1.getColor());
+		System.out.println("Circle area: " + String.format("%.3f", s1.area()));
+		System.out.println("Rectangle color: " + s2.getColor());
+		System.out.println("Rectangle area: " + String.format("%.3f", s2.area()));
 		
 	}
 	
