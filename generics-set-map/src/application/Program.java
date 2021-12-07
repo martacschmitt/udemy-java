95ab30ffc7a92c9f967034be1fd67d577786f8b6
Marta Schmitt <mcristina.schmitt@gmail.com>
2021-12-07 20:35:14 -0300

    Tipos curinga

--- generics-set-map/src/application/Program.java
+++ generics-set-map/src/application/Program.java
@@ -1,42 +1,24 @@
 package application;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Locale;
-
-import entities.Product;
-import services.CalculationService;
 
 public class Program {
 
 	public static void main(String[] args) {
+				
+		List<Integer> myInts = Arrays.asList(5, 2, 10);
+		printList(myInts);
 		
-		Locale.setDefault(Locale.US);
+		List<String> myStrs = Arrays.asList("Maria", "Alex", "Bob");
+		printList(myStrs);
 		
-		List<Product> list = new ArrayList<>();
-		
-		String path = "C:\\temp\\in.txt";
-		
-		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
-			
-			String line = br.readLine();
-			while (line != null) {
-				String[] fields = line.split(",");
-				list.add(new Product(fields[0], Double.parseDouble(fields[1])));
-				line = br.readLine();
-			}
-			
-			Product x = CalculationService.max(list);
-			System.out.println("Most expensive:");
-			System.out.println(x);
-			
-		} catch (IOException e) {
-			System.out.println("Error: " + e.getMessage());
+	}
+	
+	public static void printList(List<?> list) {
+		for (Object object : list) {
+			System.out.println(object);
 		}
-		
 	}
 	
 }
