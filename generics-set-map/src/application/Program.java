de851604580c7f2cda885e0003e4e41f4ac6f419
Marta Schmitt <mcristina.schmitt@gmail.com>
2021-12-12 09:57:27 -0300

    Curingas delimitados - Covariancia e Contravariancia

--- generics-set-map/src/application/Program.java
+++ generics-set-map/src/application/Program.java
@@ -1,35 +1,35 @@
 package application;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
-
-import entities.Circle;
-import entities.Rectangle;
-import entities.Shape;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		List<Shape> myShapes = new ArrayList<>();
-		myShapes.add(new Rectangle(3.0, 2.0));
-		myShapes.add(new Circle(2.0));
-	
-		List<Circle> myCircles = new ArrayList<>();
-		myCircles.add(new Circle(2.0));
-		myCircles.add(new Circle(3.0));
+		List<Integer> myInts = Arrays.asList(1, 2, 3, 4);
+		List<Double> myDoubles = Arrays.asList(3.14, 6.28);
+		List<Object> myObjs = new ArrayList<Object>();
 		
-		System.out.println("Total area: " + totalArea(myCircles));
+		copy(myInts, myObjs);
+		printList(myObjs);
+		copy(myDoubles, myObjs);
+		printList(myObjs);
 		
 	}
 	
-	public static double totalArea(List<? extends Shape> list) {
-		double sum = 0.0;
-		for (Shape shape : list) {
-			sum += shape.area();
+	public static void copy(List<? extends Number> source, List<? super Number> target) {
+		for (Number number : source) {
+			target.add(number);
 		}
-		return sum;
 	}
 	
-	
+	public static void printList(List<?> list) {
+		for (Object object : list) {
+			System.out.print(object + " ");
+		}
+		System.out.println();
+	}
+		
 }
