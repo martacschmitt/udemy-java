12ad914f4aefdec83d761abb033b72a0d480d0cb
Marta Schmitt <mcristina.schmitt@gmail.com>
2021-12-12 09:42:43 -0300

    Curingas delimitados

--- generics-set-map/src/application/Program.java
+++ generics-set-map/src/application/Program.java
@@ -1,24 +1,35 @@
 package application;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
+
+import entities.Circle;
+import entities.Rectangle;
+import entities.Shape;
 
 public class Program {
 
 	public static void main(String[] args) {
-				
-		List<Integer> myInts = Arrays.asList(5, 2, 10);
-		printList(myInts);
 		
-		List<String> myStrs = Arrays.asList("Maria", "Alex", "Bob");
-		printList(myStrs);
+		List<Shape> myShapes = new ArrayList<>();
+		myShapes.add(new Rectangle(3.0, 2.0));
+		myShapes.add(new Circle(2.0));
+	
+		List<Circle> myCircles = new ArrayList<>();
+		myCircles.add(new Circle(2.0));
+		myCircles.add(new Circle(3.0));
+		
+		System.out.println("Total area: " + totalArea(myCircles));
 		
 	}
 	
-	public static void printList(List<?> list) {
-		for (Object object : list) {
-			System.out.println(object);
+	public static double totalArea(List<? extends Shape> list) {
+		double sum = 0.0;
+		for (Shape shape : list) {
+			sum += shape.area();
 		}
+		return sum;
 	}
+	
 	
 }
