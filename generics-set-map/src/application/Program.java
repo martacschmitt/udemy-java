b65526373b04d0cca54b3b04a5f5997f425b4638
Marta Schmitt <mcristina.schmitt@gmail.com>
2021-12-13 20:13:41 -0300

    Set - Exercicio resolvido

--- generics-set-map/src/application/Program.java
+++ generics-set-map/src/application/Program.java
@@ -1,23 +1,44 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.time.Instant;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Scanner;
 import java.util.Set;
-import java.util.TreeSet;
 
-import entities.Product;
+import entities.LogEntry;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
-		Set<Product> set = new TreeSet<Product>();
+		Scanner sc = new Scanner(System.in);
 		
-		set.add(new Product("TV", 900.0));
-		set.add(new Product("Notebook", 1200.0));
-		set.add(new Product("Tablet", 400.0));
+		System.out.print("Enter file full path: ");
+		String path = sc.nextLine();
 		
-		for (Product product : set) {
-			System.out.println(product);
+		try (BufferedReader br = new BufferedReader(new FileReader(path))) {
+			
+			Set<LogEntry> set = new HashSet<LogEntry>();
+			
+			String line = br.readLine();
+			while (line != null) {
+				String[] fields = line.split(" ");
+				
+				set.add(new LogEntry(fields[0], Date.from(Instant.parse(fields[1]))));
+				
+				line = br.readLine();
+			}
+			System.out.println("Total users: " + set.size());
+			
+		} catch (IOException e) {
+			System.out.println("Error: " + e.getMessage());
 		}
+		
+		sc.close();
 		
 	}
 	
