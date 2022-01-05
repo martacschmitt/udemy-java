0cc53c118f7dc7b44472946ab174d1453ac192c7
Marta Schmitt <mcristina.schmitt@gmail.com>
2022-01-05 19:51:26 -0300

    Paradigma funciona de programacao - Objetos de primeira ordem

--- generics-set-map/src/application/Program.java
+++ generics-set-map/src/application/Program.java
@@ -1,25 +1,28 @@
 package application;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.List;
+
+import entities.Product;
 
 public class Program {
 
-	public static int globalValue = 3; 
+	public static int compareProducts(Product p1, Product p2) {
+		return p1.getPrice().compareTo(p2.getPrice());
+	}
 	
 	public static void main(String[] args) {
 
-		int[] vect = new int[] {3, 4, 5};
-		changeOddValues(vect);
-		System.out.println(Arrays.toString(vect));
+		List<Product> list = new ArrayList<Product>();
+		
+		list.add(new Product("TV", 900.00));
+		list.add(new Product("Notebook", 1200.00));
+		list.add(new Product("Tablet", 450.00));
+		
+		list.sort(Program::compareProducts);
+		
+		list.forEach(System.out::println);
 
-	}
-
-	public static void changeOddValues(int[] numbers) {
-		for (int i = 0; i < numbers.length; i++) {
-			if (numbers[i] % 2 != 0) {
-				numbers[i] += globalValue;
-			}
-		}
 	}
 	
 }
