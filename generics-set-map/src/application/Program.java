f8001f0dcce6d63559acf2589fce339be37ecf90
Marta Schmitt <mcristina.schmitt@gmail.com>
2022-01-05 19:45:43 -0300

    Paradigma funciona de programacao - Transparência referencial

--- generics-set-map/src/application/Program.java
+++ generics-set-map/src/application/Program.java
@@ -1,26 +1,25 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import entities.Product;
+import java.util.Arrays;
 
 public class Program {
 
+	public static int globalValue = 3; 
+	
 	public static void main(String[] args) {
 
-		List<Product> list = new ArrayList<>();
-		
-		list.add(new Product("TV", 900.00));
-		list.add(new Product("Notebook", 1200.00));
-		list.add(new Product("Tablet", 450.00));
-						
-		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
-		
-		for (Product product : list) {
-			System.out.println(product);
-		}
+		int[] vect = new int[] {3, 4, 5};
+		changeOddValues(vect);
+		System.out.println(Arrays.toString(vect));
 
 	}
 
+	public static void changeOddValues(int[] numbers) {
+		for (int i = 0; i < numbers.length; i++) {
+			if (numbers[i] % 2 != 0) {
+				numbers[i] += globalValue;
+			}
+		}
+	}
+	
 }
