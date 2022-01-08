50a57728382e8cb316954532a82d6c7ff04476d2
Marta Schmitt <mcristina.schmitt@gmail.com>
2022-01-08 17:35:54 -0300

    Predicate - Expressao lambda inline

--- programacao-funcional-expressoes-lambda/src/application/Program.java
+++ programacao-funcional-expressoes-lambda/src/application/Program.java
@@ -19,11 +19,9 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		double min = 100.0;
+		double min = 100.0;		
 		
-		Predicate<Product> pred = p -> p.getPrice() >= min;
-		
-		list.removeIf(pred);
+		list.removeIf(p -> p.getPrice() >= min);
 		
 		for (Product product : list) {
 			System.out.println(product);
