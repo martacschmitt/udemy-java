c58a47d31388a4517bf69671c0061d416be6798f
Marta Schmitt <mcristina.schmitt@gmail.com>
2022-01-08 17:46:43 -0300

    Consumer - Expressao lambda declarada

--- programacao-funcional-expressoes-lambda/src/application/Program.java
+++ programacao-funcional-expressoes-lambda/src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 import entities.Product;
@@ -20,7 +21,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		list.forEach(Product::nonStaticPriceUpdate);
+		double factor = 1.1;
+		
+		Consumer<Product> cons = p -> p.setPrice(p.getPrice() * factor);
+		
+		list.forEach(cons);
 		
 		list.forEach(System.out::println);
 
