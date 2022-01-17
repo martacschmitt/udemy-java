60e0a3a77a124166f20b826c3e56f913362634ed
Marta Schmitt <mcristina.schmitt@gmail.com>
2022-01-17 19:27:07 -0300

    Funcao como parametro

--- programacao-funcional-expressoes-lambda/src/application/Program.java
+++ programacao-funcional-expressoes-lambda/src/application/Program.java
@@ -3,10 +3,9 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 import entities.Product;
+import model.services.ProductService;
 
 public class Program {
 	
@@ -20,9 +19,11 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 		
-		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList());
-
-		names.forEach(System.out::println);
+		ProductService ps = new ProductService();
+		
+		double sum = ps.filteredSum(list, p -> p.getPrice() < 100.0);
+		
+		System.out.println("Sum = " + String.format("%.2f", sum));
 		
 	}
 	
